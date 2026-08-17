cf8da3741ddbf66254581b4c29b7c33f0bca03aa
Miguel-Matheus <resciamiguel@gmail.com>
2026-08-17 08:54:49 -0300

    Criação dos metodos Post, Put e Del e a utilização de ResponseEntity

--- src/main/java/br/com/etec/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/etec/ingresso/controller/FilmeController.java
@@ -3,6 +3,8 @@ package br.com.etec.ingresso.controller;
 import br.com.etec.ingresso.entity.Filme;
 import br.com.etec.ingresso.enums.ClassificacaoIndicativaEnum;
 import br.com.etec.ingresso.enums.SimNaoEnum;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,20 +29,41 @@ public class FilmeController {
                 .build();
         return List.of(filme1, filme2);
     }
+    List<Long> idsExistentes = List.of(1L, 2L, 3L);
     @GetMapping("/{id}")
 
-    public Filme buscarPorId(@PathVariable Long id){
-        Filme filme1 = Filme.builder()
-                .id(id)
-                .nome("Matrix")
-                .classificacao(ClassificacaoIndicativaEnum.A16)
-                .emCartaz(SimNaoEnum.S)
-                .build();
-        return filme1;
+    public ResponseEntity<Filme> buscarPorId(@PathVariable Long id){
+        if (idsExistentes.contains(id)) {
+            Filme filme1 = Filme.builder()
+                    .id(id)
+                    .nome("Matrix")
+                    .classificacao(ClassificacaoIndicativaEnum.A16)
+                    .emCartaz(SimNaoEnum.S)
+                    .build();
+            return ResponseEntity.ok(filme1);
+        }
+        return ResponseEntity.notFound().build();
     }
+
     @PostMapping
-    public Filme cadastrar(@RequestBody Filme filme){
+    public ResponseEntity<Filme> cadastrar(@RequestBody Filme filme){
         filme.setId(100L);
-        return filme;
+        return ResponseEntity.status(HttpStatus.CREATED).body(filme);
+
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Filme> atualizar(@RequestBody Filme filme,
+                           @PathVariable Long id){
+        if (idsExistentes.contains(id)) {
+            return ResponseEntity.ok(filme);
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id){
+        if (idsExistentes.contains(id)) {
+            ResponseEntity.ok().build();
+        }
+        ResponseEntity.notFound().build();
     }
 }
